791a7daa39b0a34e6c91958473984319889e49f3
Records and remembrance <goodrollings@gmail.com>
2022-02-21 10:09:02 +0900

    add icons

--- apps/client/src/pages/_app.tsx
+++ apps/client/src/pages/_app.tsx
@@ -1,28 +1,14 @@
 import type { AppProps /*, AppContext */ } from 'next/app'
-import { DefaultSeo } from 'next-seo'
-import { site } from '../constants/site'
 import { Layout } from '../component/Layout'
 import { GoogleTagManager } from '../component/GoogleTagManager'
 import { env } from '../utils/clientEnv'
+import { DefaultSEOTags } from '../component/DefaultSEOTags'
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <GoogleTagManager googleTagManagerId={env.GOOGLE_TAG_MANAGER_ID} />
-      <DefaultSeo
-        defaultTitle={site.name}
-        titleTemplate={`%s | ${site.name}`}
-        openGraph={{
-          type: 'website',
-          locale: 'ja_JP',
-          site_name: site.name
-        }}
-        twitter={{
-          handle: site.twitter,
-          site: site.twitter,
-          cardType: 'summary_large_image'
-        }}
-      />
+      <DefaultSEOTags />
       <Component {...pageProps} />
     </Layout>
   )
